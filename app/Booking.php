f2ea176c8c289a6176e1a9d6056fa5e1b9fb33be
Andrzej Rumak <ajgltl@gmail.com>
2020-04-10 16:36:25 +0200

    Model Booking, Table bookings,  Seeder and Factory for Booking, migrate&seed

---
+++ app/Booking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = ['from', 'to'];
+
+    public function bookable()
+    {
+        return $this->belongsTo(Bookable::class);
+    }
+}
