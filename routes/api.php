db3838dfd2efcdaf3e0afbe6e0fd88ac346933b3
Andrzej Rumak <ajgltl@gmail.com>
2020-04-11 07:25:14 +0200

    Single Action Controller with request input validation (Local Query Scope) returning bookable availability

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user(); 
@@ -22,3 +12,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::get('bookables/{id}', 'Api\BookableController@show'  );
 
 Route::apiResource('bookables', 'Api\BookableController');
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
