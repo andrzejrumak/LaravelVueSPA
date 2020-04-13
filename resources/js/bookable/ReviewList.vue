2c0a59efda0d494422b521c926a9ac9dfd6ba915
Andrzej Rumak <ajgltl@gmail.com>
2020-04-13 07:55:18 +0200

    Vue filters - moment.js (x day ago), Star rating component - FontAvesome implementing and logic

--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -1,46 +1,62 @@
 <template>
-  <div>
-    <h5 class="text-uppercase text-secondary font-weight-bolder pl-2 pt-4">Review List</h5>
+    <div>
+        <h5 class="text-uppercase text-secondary font-weight-bolder pl-2 pt-4">
+            Review List
+        </h5>
 
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-      <div
-        class="card px-4 my-1 border-bottom d-none d-md-block"
-        v-for="(review, index) in reviews"
-        :key="index"
-      >
-        <div class="row pt-4">
-          <div class="col-md-6">Andrzej Rumak</div>
-          <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <div
+                class="card px-4 my-1 border-bottom d-none d-md-block"
+                v-for="(review, index) in reviews"
+                :key="index"
+            >
+                <div class="row pt-4">
+                    <div class="col-md-6">Andrzej Rumak</div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <star-rating
+                            :rating="review.rating"
+                            class="fa-lg"
+                        ></star-rating>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        {{ review.created_at | fromNow }}
+                    </div>
+                </div>
+                <div class="row pt-4 pb-4">
+                    <div class="col-md-12">{{ review.content }}</div>
+                </div>
+            </div>
         </div>
-        <div class="row">
-          <div class="col-md-12">{{ review.created_at }}</div>
-        </div>
-        <div class="row pt-4 pb-4">
-          <div class="col-md-12">{{ review.content }}</div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
-  props: {
-    bookableId: String
-  },
-  data() {
-    return {
-      loading: false,
-      reviews: null
-    };
-  },
-  created() {
-    this.loading = true;
-    axios
-      .get(`/api/bookables/${this.bookableId}/reviews`)
-      .then(response => (this.reviews = response.data))
-      .then(() => (this.loading = false));
-  }
+    props: {
+        bookableId: String
+    },
+    data() {
+        return {
+            loading: false,
+            reviews: null
+        };
+    },
+    created() {
+        this.loading = true;
+        axios
+            .get(`/api/bookables/${this.bookableId}/reviews`)
+            .then(response => (this.reviews = response.data))
+            .then(() => (this.loading = false));
+    }
+    //   filters:{
+    //   fromNow(value){
+    //   return moment(value).fromNow();
+    //     }
+    //  }
 };
 </script>
