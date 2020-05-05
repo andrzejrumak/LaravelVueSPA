07eed2c6dd27881ca8929141e05a0d3bc3271869
Andrzej Rumak <ajgltl@gmail.com>
2020-05-05 08:46:28 +0200

    Bookable price (controller, migration, factory), Carbon date library, Route bookable.price.show

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityControl
     ->name('bookables.availability.show');
 Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')
     ->name('bookables.reviews.show');
+Route::get('bookables/{bookable}/price', 'Api\BookablePriceController')
+    ->name('bookable.price.show');
 
 Route::get('/booking-by-review/{reviewKey}', 'Api\BookingByReviewController')
     ->name('booking.by-review.show');    
