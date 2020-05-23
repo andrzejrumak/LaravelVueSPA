c324b2b0e6c3367fda6ce8ce98490e5e70af711d
Andrzej Rumak <ajgltl@gmail.com>
2020-05-23 23:14:09 +0200

    ui vue --auth, preparing routes, LoginController for Vue front

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user(); 
-});
 
 //Route::get('bookables', 'Api\BookableController@index'  );
 //Route::get('bookables/{id}', 'Api\BookableController@show'  );
